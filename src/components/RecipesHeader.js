6f827ac816a71369211266664e0f1dbefac5c800
s.tolstov <sergeymtolstov35@gmail.com>
2023-08-23 22:04:55 +0300

    feat: header without styles

--- src/components/RecipesHeader.js
+++ src/components/RecipesHeader.js
@@ -1,5 +1,10 @@
-function RecipesHeader() {
-    return (<></>);
+import { Link } from "react-router-dom";
+export default function RecipesHeader(){
+    return (
+        <div className="recipes-header">
+            <Link to="/favorites"><div>Favorites</div></Link>
+            <Link to="/catalog"><div>Catalog</div></Link>
+            <Link to="/"><img src={require('../assets/logo.jpg')}></img></Link>
+        </div>
+    );
 }
-
-export default RecipesHeader;
