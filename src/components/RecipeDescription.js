2389d1a080ec17f05005074dc748486f5ede3b02
s.tolstov <sergeymtolstov35@gmail.com>
2023-08-24 15:00:01 +0300

    feat: recipe description template

--- src/components/RecipeDescription.js
+++ src/components/RecipeDescription.js
@@ -1,26 +1,31 @@
 import { useParams } from "react-router-dom";
 import getRecipeId from "../utils/getRecipeId";
-import "../assets/css/RecipeDescription.css"
+import "../assets/css/RecipeDescription.css";
 
 function RecipeDescription(recipes, favRecipes) {
   const { id } = useParams();
-  const recipe = recipes.recipes.find((recipe) => getRecipeId(recipe) === id);
+  const recipe = () => {
+    let tempRecipe = {};
+    tempRecipe =
+      recipes.recipes.find((recipe) => getRecipeId(recipe) === id) ||
+      favRecipes.recipes.find((recipe) => getRecipeId(recipe) === id);
+  };
   return (
     <div className="recipe-description">
-        <img
-          className="recipe-description__image"
-          src={recipe.recipe.image}
-          alt={recipe.recipe.label}
-        />
-        <div className="recipe-description__text">
-          <h3>{recipe.recipe.label}</h3>
-          {recipe.recipe.ingredientLines.map((line) => (
-            <>
-              <p>{line}</p>
-            </>
-          ))}
-          <p> Total calories: {Math.round(recipe.recipe.calories)}</p>
-        </div>
+      <img
+        className="recipe-description__image"
+        src={recipe.recipe.image}
+        alt={recipe.recipe.label}
+      />
+      <div className="recipe-description__text">
+        <h3>{recipe.recipe.label}</h3>
+        {recipe.recipe.ingredientLines.map((line) => (
+          <>
+            <p>{line}</p>
+          </>
+        ))}
+        <p> Total calories: {Math.round(recipe.recipe.calories)}</p>
+      </div>
     </div>
   );
 }
