d8447dbcc22e23f349fc66619f90610b4fc3fba0
Dmitry Borok <134917367+dmitryborok@users.noreply.github.com>
2023-08-24 14:11:40 +0300

    RecipesHome (no search), Recipes, RecipeCard (#3)
    * RecipesHome (no search), Recipes, RecipeCard
    
    * removed typo (extra letter 'a') from Recipes.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import RecipesHome from './components/RecipesHome';
 import RecipeDescription from './components/RecipeDescription';
 import RecipesFooter from './components/RecipesFooter';
 import { useState } from 'react';
+import {dummyData} from "./utils/dummyData"; // delete when search is ready
 
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState(dummyData.hits); // change to [] when search is ready
   const [favRecipes, setFavRecipes] = useState([]);
   
   return (
     <Router>
       <RecipesHeader/>
       <Routes>
-        <Route path="/" element={<RecipesHome isFavorites={false} recipes={recipes} setRecipes={setRecipes} setFavRecipes={setFavRecipes}/>} />
-        <Route path="/catalog"  element={<RecipesHome isFavorites={false} recipes={recipes} setRecipes={setRecipes} setFavRecipes={setFavRecipes}/>} />
-        <Route path="/favorites"  element={<RecipesHome isFavorites={true} recipes={favRecipes} setRecipes={setRecipes} setFavRecipes={setFavRecipes}/>} />
+        <Route path="/" element={<RecipesHome isFavorites={false} recipes={recipes} favRecipes={favRecipes} setRecipes={setRecipes} setFavRecipes={setFavRecipes}/>} />
+        <Route path="/catalog"  element={<RecipesHome isFavorites={false} recipes={recipes} favRecipes={favRecipes} setRecipes={setRecipes} setFavRecipes={setFavRecipes}/>} />
+        <Route path="/favorites"  element={<RecipesHome isFavorites={true} recipes={favRecipes} favRecipes={favRecipes} setRecipes={setRecipes} setFavRecipes={setFavRecipes}/>} />
         <Route path="/recipe/:id"  element={<RecipeDescription recipes={recipes} favRecipes={favRecipes}/>} />
       </Routes>
       <RecipesFooter />
